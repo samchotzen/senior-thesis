aac15361bc789c1e0759a6e06ef7401fcf8febac
Sam Chotzen <schotzen23@cmc.edu>
2023-04-03 18:48:35 -0700

    commiting current files

---
+++ run_maps2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# loops over each file in the dataset and runs map.py on that file
+for file in /data/Twitter\ dataset/geoTwitter18*; do
+    nohup ./src/map.py --input_path="$file" &
+    echo $file
+for file in /data/Twitter\ dataset/geoTwitter19*; do
+    nohup ./src/map.py --input_path="$file" &
+    echo $file
+for file in /data/Twitter\ dataset/geoTwitter20*; do
+    nohup ./src/map.py --input_path="$file" &
+    echo $file
+for file in /data/Twitter\ dataset/geoTwitter21*; do
+    nohup ./src/map.py --input_path="$file" &
+    echo $file
+for file in /data/Twitter\ dataset/geoTwitter22*; do
+    nohup ./src/map.py --input_path="$file" &
+    echo $file
+done
