1ff1caeb627ea1fb58609dbc2569cf7487fb0b4e
Sam Chotzen <schotzen23@cmc.edu>
2023-04-19 16:08:51 -0700

    created yearly python file and plots

--- run_maps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# loops over each file in the dataset and runs map.py on that file
-for file in /data/Twitter\ dataset/geoTwitter*; do
-    nohup ./src/map.py --input_path="$file" &
-    echo $file
-done
